d0ea8fe0e5d6c43647dde642d474881e54577bbd
Ray Ortiz <oray@bgsu.edu>
2022-10-30 12:39:49 -0400

    user's nft layers to play with Hash's art gen

---
+++ hashlips_art_engine-1.1.2_patch_v6/src/config.js
@@ -0,0 +1,265 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "ScissorsPaperRock";
+const description =
+  "Remember that game we all use to play? Get ready to collect the (limited range) of NFT's!";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 300, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://opensea.io/SirWest",
+  creators: [
+    {
+      address: "0x701C462BF86DBc2bd0C3e77C9a53De38B9A984eD",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 50,
+    layersOrder: [
+      { name: "Background" },
+      { name: "ScissorsPaperRock" },
+      { name: "Eyes" },
+      { name: "Moustache" },
+      { name: "Mouth" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 1000,
+  height: 1000,
+  smoothing: false,
+};
+
+const gif = {
+  export: true,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 4 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {
+  seller_fee_basis_points: 300, // this defines your royalties (250 = 2.5% || 1000 = 10% || etc)
+  collection: {
+    name: "ScissorsPaperRock", // the name of your collection
+    family: "ScissorsPaperRock Gen1", //the family of your collection
+  },
+  properties: {
+    files: [
+      {
+        uri: "image.png",
+        type: "image/png",
+      },
+    ],
+    category: "image",
+    creators: [
+      {
+        address: "0x701C462BF86DBc2bd0C3e77C9a53De38B9A984eD", // the address you want to receive your payments
+        share: 100,
+      },
+    ],
+  },
+};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
+/* const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "Your Collection";
+const description = "Remember to replace this description";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 5,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
+ */
