c3a306624dcbd074289bb33dcdeb72df49fd289d
Ray Ortiz <oray@bgsu.edu>
2022-10-31 19:17:24 -0400

    working on skelton

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -3,17 +3,22 @@
 pragma solidity >=0.8.9 <0.9.0;
 
 import 'erc721a/contracts/ERC721A.sol';
-//import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
 import "hardhat/console.sol";
 
-contract DragonEyes is ERC721A, Ownable, ReentrancyGuard {
+contract RockPaperScissors is ERC721A, Ownable, ReentrancyGuard {
     using Strings for uint256;
 
-    
+  uint256 public cost;
+  uint256 public maxSupply;
+  uint256 public maxMintAmountPerTx;
+
+  string public hiddenMetadataUri;
+
  constructor(
     string memory _tokenName,
     string memory _tokenSymbol,
@@ -21,11 +26,16 @@ contract DragonEyes is ERC721A, Ownable, ReentrancyGuard {
     uint256 _maxSupply,
     uint256 _maxMintAmountPerTx,
     string memory _hiddenMetadataUri
+    
   ) ERC721A(_tokenName, _tokenSymbol) {
     cost = _cost;
     maxSupply = _maxSupply;
     maxMintAmountPerTx = _maxMintAmountPerTx;
     setHiddenMetadataUri(_hiddenMetadataUri);
   }  
+
+    function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
+  }
 
 }
