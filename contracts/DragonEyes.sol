6d169be53138766fee45c37ff7fc91248b458d70
Ray Ortiz <oray@bgsu.edu>
2022-10-31 16:55:05 -0400

    working on getting erc721a working

--- contracts/DragonEyes.sol
+++ contracts/DragonEyes.sol
@@ -1,10 +1,31 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
 
+pragma solidity >=0.8.9 <0.9.0;
+
+import 'erc721a/contracts/ERC721A.sol';
+//import "@openzeppelin/contracts/utils/Strings.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
 import "hardhat/console.sol";
 
-contract DragonEyes {
+contract DragonEyes is ERC721A, Ownable, ReentrancyGuard {
+    using Strings for uint256;
+
     
+ constructor(
+    string memory _tokenName,
+    string memory _tokenSymbol,
+    uint256 _cost,
+    uint256 _maxSupply,
+    uint256 _maxMintAmountPerTx,
+    string memory _hiddenMetadataUri
+  ) ERC721A(_tokenName, _tokenSymbol) {
+    cost = _cost;
+    maxSupply = _maxSupply;
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+    setHiddenMetadataUri(_hiddenMetadataUri);
+  }  
 
 }
