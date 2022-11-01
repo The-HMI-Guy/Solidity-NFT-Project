616402aeb6ce0520a31c66aa591fa5dd4f5e090a
Ray Ortiz <oray@bgsu.edu>
2022-11-01 08:27:49 -0400

    constructor + callerIsUser

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -13,26 +13,29 @@ import "hardhat/console.sol";
 contract RockPaperScissors is ERC721A, Ownable, ReentrancyGuard {
     using Strings for uint256;
 
-  uint256 public cost;
-  uint256 public maxSupply;
-  uint256 public maxMintAmountPerTx;
+  uint256 public immutable maxPerAddressDuringMint;
+  uint256 public immutable amountForDevs;
+
 
   string public hiddenMetadataUri;
 
- constructor(
-    string memory _tokenName,
-    string memory _tokenSymbol,
-    uint256 _cost,
-    uint256 _maxSupply,
-    uint256 _maxMintAmountPerTx,
-    string memory _hiddenMetadataUri
-    
-  ) ERC721A(_tokenName, _tokenSymbol) {
-    cost = _cost;
-    maxSupply = _maxSupply;
-    maxMintAmountPerTx = _maxMintAmountPerTx;
-    setHiddenMetadataUri(_hiddenMetadataUri);
-  }  
+  constructor(
+    uint256 maxBatchSize_,
+    uint256 collectionSize_,
+    uint256 amountForDevs_
+  ) ERC721A("RockPaperScissors", "RPS", maxBatchSize_, collectionSize_) {
+    maxPerAddressDuringMint = maxBatchSize_;
+    amountForDevs = amountForDevs_;
+  }
+    /**
+     * @dev Ensure a Smart Contract is not interfacing with this contract.
+     * msg.sender can either be an account address or smart contract address, while
+     * tx.origin will always be the account/wallet address.
+     */
+  modifier callerIsUser() {
+    require(tx.origin == msg.sender, "The caller is another contract");
+    _;
+  }
 
     function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
     hiddenMetadataUri = _hiddenMetadataUri;
