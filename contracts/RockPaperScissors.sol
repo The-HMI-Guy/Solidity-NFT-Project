bc9e73fd0509676b9643f0f4ffd50c24b7c9d7d1
Ray Ortiz <oray@bgsu.edu>
2022-11-09 07:37:37 -0500

    contract compiler updated to 0.8.10

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -11,222 +11,200 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
 contract RockPaperScissors is ERC721AQueryable, Ownable, ReentrancyGuard {
-    using Strings for uint256;
+  using Strings for uint256;
 
-    // =============================================================
-    //                          VARIABLES
-    // =============================================================
-    uint256 public immutable maxMintAmountPerTx;
-    bytes32 public immutable merkleRoot;
+  // =============================================================
+  //                          CONSTANTS
+  // =============================================================
+  uint public constant MAX_SUPPLY = 777;
 
-    uint public price;
-    uint public wlPrice;
+  // =============================================================
+  //                          VARIABLES
+  // =============================================================
+  uint256 public immutable maxMintAmountPerTx;
+  bytes32 public immutable merkleRoot;
 
-    bool public paused = true;
-    bool public whitelistMintEnabled = false;
-    bool public revealed = false;
+  uint public price;
+  uint public wlPrice;
 
-    // mapping(address => uint256) public allowlist;
-    mapping(address => uint) public whitelistClaimed;
+  bool public paused = true;
+  bool public whitelistMintEnabled = false;
+  bool public revealed = false;
 
-    // =============================================================
-    //                          METADATA
-    // =============================================================
-    string public uriPrefix = "";
-    string public uriSuffix = ".json";
-    string public hiddenMetadataUri;
-    string private _baseTokenURI;
+  // mapping(address => uint256) public allowlist;
+  mapping(address => uint) public whitelistClaimed;
 
-    // =============================================================
-    //                          CONSTRUCTOR
-    // =============================================================
-    constructor(
-        uint256 _maxMintAmountPerTx,
-        uint _collectionSize,
-        uint _price,
-        string memory _hiddenMetadataUri,
-        bytes32 _merkleRoot
-    )
-        ERC721A(
-            "RockPaperScissors",
-            "RPS",
-            _maxMintAmountPerTx,
-            _collectionSize
+  // =============================================================
+  //                          METADATA
+  // =============================================================
+  string public uriPrefix = "";
+  string public uriSuffix = ".json";
+  string public hiddenMetadataUri;
+  string private _baseTokenURI;
+
+  // =============================================================
+  //                          CONSTRUCTOR
+  // =============================================================
+  constructor(
+    uint256 _maxMintAmountPerTx,
+    uint _price,
+    string memory _hiddenMetadataUri,
+    bytes32 _merkleRoot
+  ) ERC721A("RockPaperScissors", "RPS") {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
+    setCost(_price);
+    setHiddenMetadataUri(_hiddenMetadataUri);
+    merkleRoot = _merkleRoot;
+  }
+
+  // =============================================================
+  //                          INTERNAL MODIFIERS
+  // =============================================================
+
+  /**
+   * @dev Ensure a Smart Contract is not interfacing with this contract.
+   * msg.sender can either be an account address or smart contract address, while
+   * tx.origin will always be the account/wallet address.
+   */
+  modifier callerIsUser() {
+    require(tx.origin == msg.sender, "The caller is another contract");
+    _;
+  }
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(
+      _mintAmount > 0 && _mintAmount <= maxMintAmountPerTx,
+      "Invalid mint amount!"
+    );
+    require(totalSupply() + _mintAmount <= MAX_SUPPLY, "Max supply exceeded!");
+    _;
+  }
+
+  modifier mintPriceCompliance(uint256 _mintAmount) {
+    require(msg.value >= price * _mintAmount, "Insufficient funds!");
+    _;
+  }
+
+  // =============================================================
+  //                          INTERNAL MINT LOGIC
+  // =============================================================
+
+  function mint(
+    uint256 _mintAmount
+  )
+    public
+    payable
+    callerIsUser
+    mintCompliance(_mintAmount)
+    mintPriceCompliance(_mintAmount)
+  {
+    require(!paused, "The contract is paused!");
+
+    _safeMint(msg.sender, _mintAmount);
+  }
+
+  function whitelistMint(
+    uint256 _mintAmount,
+    bytes32[] calldata _merkleProof
+  ) public payable {
+    require(whitelistMintEnabled, "The whitelist sale is not enabled!");
+    require(whitelistClaimed[msg.sender] > 2, "Address already claimed!");
+    bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+    require(
+      MerkleProof.verify(_merkleProof, merkleRoot, leaf),
+      "Invalid proof!"
+    );
+
+    whitelistClaimed[msg.sender] += _mintAmount;
+    _safeMint(msg.sender, _mintAmount);
+  }
+
+  function airdrop(address wallet, uint256 amount) external onlyOwner {
+    require(totalSupply() + amount < MAX_SUPPLY + 1, "exceed max supply");
+
+    _safeMint(wallet, amount);
+  }
+
+  function refundIfOver(uint256 _price) private {
+    require(msg.value >= _price, "Need to send more ETH.");
+    if (msg.value > _price) {
+      payable(msg.sender).transfer(msg.value - _price);
+    }
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return _baseTokenURI;
+  }
+
+  function withdrawMoney() external onlyOwner nonReentrant {
+    (bool success, ) = msg.sender.call{ value: address(this).balance }("");
+    require(success, "Transfer failed.");
+  }
+
+  function numberMinted(address owner) public view returns (uint256) {
+    return _numberMinted(owner);
+  }
+
+  function _startTokenId() internal view virtual override returns (uint256) {
+    return 1;
+  }
+
+  function tokenURI(
+    uint256 _tokenId
+  ) public view virtual override(ERC721A) returns (string memory) {
+    require(
+      _exists(_tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
+    if (revealed == false) {
+      return hiddenMetadataUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return
+      bytes(currentBaseURI).length > 0
+        ? string(
+          abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix)
         )
-    {
-        maxMintAmountPerTx = _maxMintAmountPerTx;
-        setCost(cost_);
-        setHiddenMetadataUri(_hiddenMetadataUri);
-        merkleRoot = _merkleRoot;
-    }
+        : "";
+  }
 
-    // =============================================================
-    //                          INTERNAL MODIFIERS
-    // =============================================================
+  function setCost(uint _price) public onlyOwner {
+    price = _price;
+  }
 
-    /**
-     * @dev Ensure a Smart Contract is not interfacing with this contract.
-     * msg.sender can either be an account address or smart contract address, while
-     * tx.origin will always be the account/wallet address.
-     */
-    modifier callerIsUser() {
-        require(tx.origin == msg.sender, "The caller is another contract");
-        _;
-    }
-    modifier mintCompliance(uint256 _mintAmount) {
-        require(
-            _mintAmount > 0 && _mintAmount <= maxMintAmountPerTx,
-            "Invalid mint amount!"
-        );
-        require(
-            totalSupply() + _mintAmount <= collectionSize,
-            "Max supply exceeded!"
-        );
-        _;
-    }
+  function setWLPrice(uint amount) external onlyOwner {
+    wlPrice = amount;
+  }
 
-    modifier mintPriceCompliance(uint256 _mintAmount) {
-        require(msg.value >= price * _mintAmount, "Insufficient funds!");
-        _;
-    }
+  function setPaused(bool _state) public onlyOwner {
+    paused = _state;
+  }
 
-    // =============================================================
-    //                          INTERNAL MINT LOGIC
-    // =============================================================
+  function setWhitelistMintEnabled(bool _state) public onlyOwner {
+    whitelistMintEnabled = _state;
+  }
 
-    function mint(
-        uint256 _mintAmount
-    )
-        public
-        payable
-        callerIsUser
-        mintCompliance(_mintAmount)
-        mintPriceCompliance(_mintAmount)
-    {
-        require(!paused, "The contract is paused!");
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
+  }
 
-        _safeMint(msg.sender, _mintAmount);
-    }
+  function setHiddenMetadataUri(
+    string memory _hiddenMetadataUri
+  ) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
+  }
 
-    function whitelistMint(
-        uint256 _mintAmount,
-        bytes32[] calldata _merkleProof
-    ) public payable {
-        require(whitelistMintEnabled, "The whitelist sale is not enabled!");
-        require(whitelistClaimed[msg.sender] > 2, "Address already claimed!");
-        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
-        require(
-            MerkleProof.verify(_merkleProof, merkleRoot, leaf),
-            "Invalid proof!"
-        );
+  function setBaseURI(string calldata baseURI) external onlyOwner {
+    _baseTokenURI = baseURI;
+  }
 
-        whitelistClaimed[msg.sender] += _mintAmount;
-        _safeMint(msg.sender, _mintAmount);
-    }
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
+  }
 
-    function airdrop(address wallet, uint256 amount) external onlyOwner {
-        require(totalSupply() + amount < MAX_SUPPLY + 1, "exceed max supply");
-
-        _safeMint(wallet, amount);
-    }
-
-    function refundIfOver(uint256 _price) private {
-        require(msg.value >= _price, "Need to send more ETH.");
-        if (msg.value > _price) {
-            payable(msg.sender).transfer(msg.value - _price);
-        }
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseTokenURI;
-    }
-
-    function withdrawMoney() external onlyOwner nonReentrant {
-        (bool success, ) = msg.sender.call{value: address(this).balance}("");
-        require(success, "Transfer failed.");
-    }
-
-    function numberMinted(address owner) public view returns (uint256) {
-        return _numberMinted(owner);
-    }
-
-    function _startTokenId() internal view virtual override returns (uint256) {
-        return 1;
-    }
-
-    function tokenURI(
-        uint256 _tokenId
-    ) public view virtual override returns (string memory) {
-        require(
-            _exists(_tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-
-        if (revealed == false) {
-            return hiddenMetadataUri;
-        }
-
-        string memory currentBaseURI = _baseURI();
-        return
-            bytes(currentBaseURI).length > 0
-                ? string(
-                    abi.encodePacked(
-                        currentBaseURI,
-                        _tokenId.toString(),
-                        uriSuffix
-                    )
-                )
-                : "";
-    }
-
-    function getOwnershipData(
-        uint256 tokenId
-    ) external view returns (TokenOwnership memory) {
-        return ownershipOf(tokenId);
-    }
-
-    function setOwnersExplicit(
-        uint256 quantity
-    ) external onlyOwner nonReentrant {
-        _setOwnersExplicit(quantity);
-    }
-
-    function setCost(uint _price) public onlyOwner {
-        price = _price;
-    }
-
-    function setWLPrice(uint amount) external onlyOwner {
-        wlPrice = amount;
-    }
-
-    function setPaused(bool _state) public onlyOwner {
-        paused = _state;
-    }
-
-    function setWhitelistMintEnabled(bool _state) public onlyOwner {
-        whitelistMintEnabled = _state;
-    }
-
-    function setRevealed(bool _state) public onlyOwner {
-        revealed = _state;
-    }
-
-    function setHiddenMetadataUri(
-        string memory _hiddenMetadataUri
-    ) public onlyOwner {
-        hiddenMetadataUri = _hiddenMetadataUri;
-    }
-
-    function setBaseURI(string calldata baseURI) external onlyOwner {
-        _baseTokenURI = baseURI;
-    }
-
-    function setUriPrefix(string memory _uriPrefix) public onlyOwner {
-        uriPrefix = _uriPrefix;
-    }
-
-    function setUriSuffix(string memory _uriSuffix) public onlyOwner {
-        uriSuffix = _uriSuffix;
-    }
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
+  }
 }
 //["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"],["0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"],["0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"]
