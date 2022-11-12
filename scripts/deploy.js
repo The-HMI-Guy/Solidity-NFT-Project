35c25f06b9d175d8a0dda74e7ef22ca2480af078
Ray Ortiz <oray@bgsu.edu>
2022-11-12 15:35:27 -0500

    deploy script updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  // Get the contract to deploy and deploy.
+  const RockPaperScissors = await hre.ethers.getContractFactory(
+    "RockPaperScissors"
+  );
+  const rockPaperScissorsContract = await RockPaperScissors.deploy(
+    5,
+    ethers.BigNumber.from("1000000000000000000"), //1ETH
+    ethers.BigNumber.from("1000000000000000000"), //1ETH
+    "ipfs://QmZD66sq4Em1xQKSchg45q2AtTf8qts5LZMPx3kCqPYpQg/hidden.png",
+    ethers.BigNumber.from(
+      "0x04c8b7dc04c57c4e8e83fff68c0e603c2871484788a158e8c63854ecd5d256ee"
+    )
+  );
+  await rockPaperScissorsContract.deployed();
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "RockPaperScissors deployed to ",
+    rockPaperScissorsContract.address
   );
 }
 
