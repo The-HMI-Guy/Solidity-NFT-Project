69be0ed82d609f1d935a90680aabf2c59bd8a081
Ray Ortiz <oray@bgsu.edu>
2022-11-12 15:34:25 -0500

    file was needed to send arguments when verifying

---
+++ arguments.js
@@ -0,0 +1,9 @@
+module.exports = [
+  5,
+  ethers.BigNumber.from("1000000000000000000"), //1ETH
+  ethers.BigNumber.from("1000000000000000000"), //1ETH
+  "ipfs://QmZD66sq4Em1xQKSchg45q2AtTf8qts5LZMPx3kCqPYpQg/hidden.png",
+  ethers.BigNumber.from(
+    "0x04c8b7dc04c57c4e8e83fff68c0e603c2871484788a158e8c63854ecd5d256ee"
+  ),
+];
