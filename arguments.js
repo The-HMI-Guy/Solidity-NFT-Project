aa3df177406f5a1352dd2703c64e8baf714b83e7
Ray Ortiz <oray@bgsu.edu>
2022-11-15 20:49:55 -0500

    hiddenURI string updated

--- arguments.js
+++ arguments.js
@@ -1,8 +1,8 @@
 module.exports = [
   5,
-  ethers.BigNumber.from("1000000000000000000"), //1ETH
-  ethers.BigNumber.from("1000000000000000000"), //1ETH
-  "ipfs://QmZD66sq4Em1xQKSchg45q2AtTf8qts5LZMPx3kCqPYpQg/hidden.png",
+  ethers.BigNumber.from("1000000000000000000"),
+  ethers.BigNumber.from("1000000000000000000"),
+  "ipfs://QmcmJ1g8kNsiRRKUznLkjcigFk4sw7YZMy8dQ7W9axNNGN/hidden.json",
   ethers.BigNumber.from(
     "0x04c8b7dc04c57c4e8e83fff68c0e603c2871484788a158e8c63854ecd5d256ee"
   ),
