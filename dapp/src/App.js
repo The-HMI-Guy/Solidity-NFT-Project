426d6c905e1eba3c4eebbeafd47928eea6c1c923
Ray Ortiz <oray@bgsu.edu>
2022-11-19 15:49:48 -0500

    removed skelton code, installed ethers, and mods

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 function App() {
+  const [account, setAccount] = useState(null); //State Variable - when a state variable is changes, the app will re-render
+  const initConnection = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } else {
+      console.log("Please Install Metamask.");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={initConnection}>Connect</button>
+      <p>{account}</p>
     </div>
   );
 }
