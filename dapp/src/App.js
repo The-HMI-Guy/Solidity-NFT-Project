6dd159af1bc9ffd1fb7fb4e2ff177712d8404931
Ray Ortiz <oray@bgsu.edu>
2022-11-20 11:51:57 -0500

    OS logo, react-icons, style mods

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import { ethers } from "ethers";
 import { useState, useEffect } from "react";
+import { FaEthereum} from "react-icons/fa";
 
 function App() {
-  const [account, setAccount] = useState(null); //State Variable - when a state variable is changes, the app will re-render
+  const [account, setAccount] = useState(""); //State Variable - when a state variable is changes, the app will re-render
   const initConnection = async () => {
     if (typeof window.ethereum !== "undefined") {
       const accounts = await window.ethereum.request({
@@ -19,11 +20,25 @@ function App() {
   }, []);
   return (
     <div className="page">
-      <div>
-        <img />
-        <p>123</p>
-        <button onClick={initConnection}>Connect</button>
-        <p>{account}</p>
+      <div className="header">
+        <img
+          src={require(`./assets/images/OS-Blue.png`)}
+          alt="OpenSea"
+          className="osIcon"
+        />
+        <p>
+          123{" "}
+          <span>
+            <FaEthereum style={{marginLeft: "5px"}}/>
+          </span>
+        </p>
+        {account === "" ? ( //ternary operator
+          <button onClick={initConnection} className="button">
+            Connect
+          </button>
+        ) : (
+          <p>...{account.substring(account.length - 7)}</p>
+        )}
       </div>
     </div>
   );
