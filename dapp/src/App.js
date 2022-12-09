351b474c9fbd9a3be5859c818715bfe29a80777d
Ray Ortiz <oray@bgsu.edu>
2022-12-09 09:31:43 -0500

    Getting rainbowkit to work

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -3,7 +3,7 @@ import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 import { FaEthereum } from "react-icons/fa";
 import abi from "./abi/abi.json";
-
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
   const [account, setAccount] = useState(""); //State Variable - when a state variable is changes, the app will re-render
@@ -66,60 +66,54 @@ function App() {
 
   return (
     <div className="page">
-    <div className="header">
-      <img
-        src={require(`./assets/images/ape.png`)}
-        alt="hmiLogo"
-        className="hmiIcon"
-      />
-      <a
-        href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <div className="header">
         <img
-          src={require(`./assets/images/OS-Blue.png`)}
-          alt="OpenSea"
-          className="osIcon"
+          src={require(`./assets/images/ape.png`)}
+          alt="hmiLogo"
+          className="hmiIcon"
         />
-      </a>
+        <a
+          href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={require(`./assets/images/OS-Blue.png`)}
+            alt="OpenSea"
+            className="osIcon"
+          />
+        </a>
 
-      <p>
-        Total Supply {totalSup.toString()} /{" "}
-        {contractInfo.maxSupply.toString()}
-      </p>
-      <p>
-        Price {contractInfo.price}
-        <span>
-          <FaEthereum style={{ marginLeft: "5px" }} />
-        </span>
-      </p>
-      {account === "" ? ( //ternary operator
-        <button onClick={initConnection} className="button">
-          Connect
-        </button>
-      ) : (
         <p>
-          {account.slice(0, 5)}...{account.substring(account.length - 4)}
+          Total Supply {totalSup.toString()} /{" "}
+          {contractInfo.maxSupply.toString()}
         </p>
-      )}
+        <p>
+          Price {contractInfo.price}
+          <span>
+            <FaEthereum style={{ marginLeft: "5px" }} />
+          </span>
+        </p>
+        <div>
+          <ConnectButton></ConnectButton>
+        </div>
+      </div>
+
+      <div>
+        <button
+          style={{
+            backgroundColor: "red",
+            color: "white",
+            fontSize: 16,
+            padding: "10px 20px",
+            borderRadius: 4,
+          }}
+        >
+          Mint!
+        </button>
+      </div>
     </div>
-    <div>
-      <button
-        style={{
-          backgroundColor: "red",
-          color: "white",
-          fontSize: 16,
-          padding: "10px 20px",
-          borderRadius: 4,
-        }}
-      >
-        Mint!
-      </button>
-    </div>
-  </div> 
   );
 }
 
 export default App;
-
