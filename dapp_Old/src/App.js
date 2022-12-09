480db9bd5ed38b9be7b198a872281233d9a64de7
Ray Ortiz <oray@bgsu.edu>
2022-12-09 08:00:36 -0500

    Move folder location

--- dapp_Old/src/App.js
+++ dapp_Old/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { FaEthereum } from "react-icons/fa";
 import abi from "./abi/abi.json";
 
+
 function App() {
   const [account, setAccount] = useState(""); //State Variable - when a state variable is changes, the app will re-render
   const [totalSup, setTotalSup] = useState("");
@@ -65,51 +66,60 @@ function App() {
 
   return (
     <div className="page">
-      <div className="header">
+    <div className="header">
+      <img
+        src={require(`./assets/images/ape.png`)}
+        alt="hmiLogo"
+        className="hmiIcon"
+      />
+      <a
+        href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
-          src={require(`./assets/images/ape.png`)}
-          alt="hmiLogo"
-          className="hmiIcon"
+          src={require(`./assets/images/OS-Blue.png`)}
+          alt="OpenSea"
+          className="osIcon"
         />
-        <a
-          href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={require(`./assets/images/OS-Blue.png`)}
-            alt="OpenSea"
-            className="osIcon"
-          />
-        </a>
+      </a>
 
-        <p>
-          Total Supply {totalSup.toString()} /{" "}
-          {contractInfo.maxSupply.toString()}
-        </p>
-        <p>
-          Price {contractInfo.price}
-          <span>
-            <FaEthereum style={{ marginLeft: "5px" }} />
-          </span>
-        </p>
-        {account === "" ? ( //ternary operator
-          <button onClick={initConnection} className="button">
-            Connect
-          </button>
-        ) : (
-          <p>
-            {account.slice(0, 5)}...{account.substring(account.length - 4)}
-          </p>
-        )}
-      </div>
-      <div>
-        <button onClick={mintNFTs} className="mintBtn">
-          Mint
+      <p>
+        Total Supply {totalSup.toString()} /{" "}
+        {contractInfo.maxSupply.toString()}
+      </p>
+      <p>
+        Price {contractInfo.price}
+        <span>
+          <FaEthereum style={{ marginLeft: "5px" }} />
+        </span>
+      </p>
+      {account === "" ? ( //ternary operator
+        <button onClick={initConnection} className="button">
+          Connect
         </button>
-      </div>
+      ) : (
+        <p>
+          {account.slice(0, 5)}...{account.substring(account.length - 4)}
+        </p>
+      )}
     </div>
+    <div>
+      <button
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          fontSize: 16,
+          padding: "10px 20px",
+          borderRadius: 4,
+        }}
+      >
+        Mint!
+      </button>
+    </div>
+  </div> 
   );
 }
 
 export default App;
+
