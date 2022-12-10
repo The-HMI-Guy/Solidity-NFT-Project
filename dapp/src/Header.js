1d85eaf938612bbe1377bbfd3637aa955c12fa22
Ray Ortiz <oray@bgsu.edu>
2022-12-10 11:00:15 -0500

    adding Card to the header

--- dapp/src/Header.js
+++ dapp/src/Header.js
@@ -1,6 +1,9 @@
 import { useContractRead } from "wagmi";
 import contractInterface from "./abi/abi.json";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button, Card, Image } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+
 const Header = () => {
   const { data: MaxSupply } = useContractRead({
     address: "0x58a56731d3177eec6e395b4397c00f6e1a1436a8",
@@ -24,26 +27,43 @@ const Header = () => {
   });
   return (
     <div>
-      <p>Contract Status:{status.toString()}</p>
-      <p>Whitelist Status:{wlStatus.toString()}</p>
-      <p>Total Minted:{totalSupply.toString()}</p>
-      <p>Total Supply:{MaxSupply.toString()}</p>
       <div className="btnConnect">
         <ConnectButton></ConnectButton>
       </div>
-      <div className="os">
-        OpeanSea
-        <a
-          href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={require(`./assets/images/OS-Blue.png`)}
-            alt="OpenSea"
-            className="osIcon"
-          />
-        </a>
+
+      <div className="card">
+        <Card.Group>
+          <Card>
+            <Card.Content>
+              <Image
+                floated="right"
+                size="mini"
+                src={require(`./assets/images/OS-Blue.png`)}
+                href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
+                target="_blank"
+                rel="noreferrer"
+              />
+              <Card.Header>Steve Sanders</Card.Header>
+              <Card.Meta>Friends of Elliot</Card.Meta>
+              <Card.Description>
+                Contract Status: {status.toString()}
+                <p>Whitelist Status: {wlStatus.toString()}</p>
+                <p>Total Minted: {totalSupply.toString()}</p>
+                Total Supply: {MaxSupply.toString()}
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button basic color="green">
+                  Approve
+                </Button>
+                <Button basic color="red">
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        </Card.Group>
       </div>
     </div>
   );
