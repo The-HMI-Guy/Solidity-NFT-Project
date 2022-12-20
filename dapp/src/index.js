c5e2092f5da581ade3f0d91a479e8271137b1ab3
Ray Ortiz <oray@bgsu.edu>
2022-12-20 11:57:32 -0500

    npx thirdweb@latest create app --evm

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -1,39 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import reportWebVitals from "./reportWebVitals";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
 
-//const ALCHEMY_API_KEY = process.env.REACT_APP_ALCHEMY_KEY
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
 
-const { chains, provider } = configureChains(
-  [chain.goerli],
-  [
-    alchemyProvider({ apiKey: "C-OuQm9rwjEdE-tuVodh8xmicBtZ3hXI" }),
-    publicProvider(),
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: "RPS App",
-  chains,
-});
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider coolMode chains={chains}>
-        <App />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <App />
+    </ThirdwebProvider>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
