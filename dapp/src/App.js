3b8a6d41fe6616e355f16fa7d482341b299e3aed
Ray Ortiz <oray@bgsu.edu>
2022-12-21 07:27:47 -0500

    thirdweb bug resolved. useNFTS hook was the issue

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,58 +1,78 @@
-import { ConnectWallet, useContract } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  useContract,
+  ThirdwebNftMedia,
+  useNFT,
+  useAddress,
+  useContractRead,
+} from "@thirdweb-dev/react";
 import "./styles/Home.css";
 
 const contractAddress = "0x58a56731D3177eeC6e395B4397c00F6E1A1436a8";
 
 export default function Home() {
-  const { contract, isLoading, error } = useContract(contractAddress);
+  const address = useAddress();
+  const { contract, error } = useContract(contractAddress);
+  const { data: nft, isLoading } = useNFT(contract, 1);
+  const { data: name, isLoading: loadingName } = useContractRead(
+    contract,
+    "name" // The name of the view/mapping/variable on your contract
+  );
 
   return (
-    <div>
-      <ConnectWallet />
-      {contract?.name}
-    </div>
-
-    // <div className="container">
-    //   <main className="main">
-    //     <h1 className="title">
-    //       Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-    //     </h1>
-
-    //     <p className="description">
-    //       Get started by configuring your desired network in{" "}
-    //       <code className="code">src/index.js</code>, then modify the{" "}
-    //       <code className="code">src/App.js</code> file!
-    //     </p>
-
-    //     <div className="connect">
-    //       <ConnectWallet />
-    //     </div>
-
-    //     <div className="grid">
-    //       <a href="https://portal.thirdweb.com/" className="card">
-    //         <h2>Portal &rarr;</h2>
-    //         <p>
-    //           Guides, references and resources that will help you build with
-    //           thirdweb.
-    //         </p>
-    //       </a>
-
-    //       <a href="https://thirdweb.com/dashboard" className="card">
-    //         <h2>Dashboard &rarr;</h2>
-    //         <p>
-    //           Deploy, configure and manage your smart contracts from the
-    //           dashboard.
-    //         </p>
-    //       </a>
-
-    //       <a href="https://portal.thirdweb.com/templates" className="card">
-    //         <h2>Templates &rarr;</h2>
-    //         <p>
-    //           Discover and clone template projects showcasing thirdweb features.
-    //         </p>
-    //       </a>
-    //     </div>
-    //   </main>
+    // <div>
+    //   <ConnectWallet />
+    //   <p>{name}</p>
+    //   {!isLoading && nft ? (
+    //     <ThirdwebNftMedia metadata={nft.metadata} />
+    //   ) : (
+    //     <p>Loading...</p>
+    //   )}
     // </div>
+
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
+          Welcome to the Rock Paper Scissor NFT!
+        </h1>
+
+        <p className="description">
+          {!isLoading && nft ? (
+            <ThirdwebNftMedia metadata={nft.metadata} />
+          ) : (
+            <p>Loading...</p>
+          )}
+        </p>
+
+        <div className="connect">
+          <ConnectWallet />
+        </div>
+
+        <div className="grid">
+          <a href="https://portal.thirdweb.com/" className="card">
+            <h2>Portal &rarr;</h2>
+            <p>
+              Guides, references and resources that will help you build with
+              thirdweb.
+            </p>
+          </a>
+
+          <a href="https://thirdweb.com/dashboard" className="card">
+            <h2>Dashboard &rarr;</h2>
+            <p>
+              Deploy, configure and manage your smart contracts from the
+              dashboard.
+            </p>
+          </a>
+
+          <a href="https://portal.thirdweb.com/templates" className="card">
+            <h2>Templates &rarr;</h2>
+            <p>
+              Discover and clone template projects showcasing thirdweb features.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
   );
 }
