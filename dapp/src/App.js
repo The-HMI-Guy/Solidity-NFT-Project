03bd5d09959bd40c66279c957c78942f07936c38
Ray Ortiz <oray@bgsu.edu>
2022-12-24 11:00:56 -0500

    styling tweaks

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -5,32 +5,10 @@ export default function Home() {
   return (
     <div className="container">
       <main className="main">
-        <h1 className="title">Welcome to the Rock Paper Scissor NFT Minting Dapp!</h1>
+        <h1 className="title">
+          <p>Welcome to the Rock Paper Scissor NFT Minting Dapp!</p>
+        </h1>
         <Mint />
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
       </main>
     </div>
   );
--- dapp/src/components/mint.js
+++ dapp/src/components/mint.js
@@ -1,11 +1,10 @@
 import {
-  ConnectWallet,
   useContract,
   useContractRead,
   Web3Button,
-  useAddress
+  useAddress,
 } from "@thirdweb-dev/react";
-import { BigNumber, ethers } from "ethers";
+import { ethers } from "ethers";
 import "../styles/Home.css";
 import etherscanImage from "../assets/images/etherscan.png";
 import openseaImage from "../assets/images/OS-Blue.png";
@@ -16,6 +15,10 @@ const contractAddress = "0x58a56731D3177eeC6e395B4397c00F6E1A1436a8";
 const Mint = () => {
   const { contract } = useContract(contractAddress);
   const address = useAddress();
+
+  // =============================================================
+  //                          CONTRACT READS
+  // =============================================================
   const { data: price, isLoading: loadingPrice } = useContractRead(
     contract,
     "price"
@@ -34,10 +37,16 @@ const Mint = () => {
       <img alt="nft gif" src={previewGIF}></img>
 
       <div className="price">
-        <p>Price Per NFT: {price ? ethers.utils.formatEther(price.toString()) : 0} ETH</p>
+        <p>
+          Price Per NFT:{" "}
+          {price ? ethers.utils.formatEther(price.toString()) : 0} ETH
+        </p>
       </div>
       <div className="status">
-        <p>Minting Status: {paused ? "Not Active" : "Active" } Whitelist Status: {wlEnabled ? "Active" : "Not Active" } Add: {address}</p>
+        <p>
+          Minting Status: {paused ? "Not Active" : "Active"} Whitelist Status:{" "}
+          {wlEnabled ? "Active" : "Not Active"} Add: {address}
+        </p>
       </div>
       <div className="connect">
         <Web3Button
@@ -55,6 +64,8 @@ const Mint = () => {
           href="https://goerli.etherscan.io/address/0x58a56731D3177eeC6e395B4397c00F6E1A1436a8"
           className="card"
         >
+          {" "}
+          <h2>Contract</h2>
           <img
             width={"100px"}
             height={"100px"}
@@ -67,6 +78,7 @@ const Mint = () => {
           href="https://testnets.opensea.io/collection/rockpaperscissors-9vlkhasx9z"
           className="card"
         >
+          <h2>Opensea</h2>
           <img
             width={"100px"}
             height={"100px"}
